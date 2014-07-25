55c0bee125bbc31e751df3142c7e8a5caeda55b0
Conrado <conrado@Conrados-MacBook-Pro.local>
2014-07-25 17:13:13 -0300

    Initial Commit

---
+++ promonster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  promonster
+//
+//  Created by Conrado on 25/07/14.
+//  Copyright (c) 2014 ConradoCarneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ promonsterTests/promonsterTests.m
@@ -0,0 +1,34 @@
+//
+//  promonsterTests.m
+//  promonsterTests
+//
+//  Created by Conrado on 25/07/14.
+//  Copyright (c) 2014 ConradoCarneiro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface promonsterTests : XCTestCase
+
+@end
+
+@implementation promonsterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
